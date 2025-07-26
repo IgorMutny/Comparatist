4ab2aaea85eb52bb1728269e3bd1a5c41b836180
IgorMutny <igor.mutny@gmail.com>
2025-07-27 00:41:40 +0700

    Refactoring 3

---
+++ View/FileService.cs
@@ -0,0 +1,57 @@
+﻿namespace Comparatist
+{
+    public class FileService
+    {
+        private const string Extension = ".comparatist";
+
+        private Database _db = new();
+        private string _filePath = string.Empty;
+        private Action _onLoaded;
+
+        public FileService(Database db, Action onLoaded)
+        {
+            _db = db;
+            _onLoaded = onLoaded;
+        }
+
+        public void Open()
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                dialog.Filter = $"Comparatist DataBase files (*{Extension})|*{Extension}";
+                dialog.Title = "Select Comparatist DataBase file";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    _filePath = dialog.FileName;
+                    _db.Load(_filePath);
+                    _onLoaded();
+                }
+            }
+        }
+
+        public void SaveAs()
+        {
+            using (var dialog = new SaveFileDialog())
+            {
+                dialog.Filter = $"Comparatist DataBase files (*{Extension})|*{Extension}";
+                dialog.Title = "Save Comparatist DataBase as...";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    string path = dialog.FileName;
+                    _db.Save(path);
+                    _filePath = path;
+                }
+            }
+        }
+
+        public void Save()
+        {
+            if (string.IsNullOrEmpty(_filePath))
+                SaveAs();
+            else
+                _db.Save(_filePath);
+        }
+    }
+}
--- View/MainForm.cs
+++ View/MainForm.cs
@@ -2,11 +2,9 @@ namespace Comparatist
 {
     public partial class MainForm : Form
     {
-        private const string Extension = ".comparatist";
-
         private Database _db = new();
-        private string _filePath = string.Empty;
         private ContentHolderTypes _currentContentHolder = ContentHolderTypes.Roots;
+        private FileService _fileService;
         private LanguagesService _languagesService;
         private SourcesService _sourcesService;
         private SemanticTreeService _semanticTreeService;
@@ -15,6 +13,7 @@ namespace Comparatist
         {
             _db = new();
             InitializeComponent();
+            _fileService = new FileService(_db, RefreshAllContent);
             _languagesService = new(_languagesGridView, _db.Languages);
             _sourcesService = new(_sourcesGridView, _db.Sources);
             _semanticTreeService = new(
@@ -25,123 +24,15 @@ namespace Comparatist
                 (obj, e) => DoDragDrop(obj, e));
         }
 
-        #region FILE_OPERATIONS
-        private void Open(object sender, EventArgs e)
-        {
-            using (var dialog = new OpenFileDialog())
-            {
-                dialog.Filter = $"Comparatist DataBase files (*{Extension})|*{Extension}";
-                dialog.Title = "Select Comparatist DataBase file";
+        private void Open(object sender, EventArgs e) => _fileService.Open();
+        private void SaveAs(object sender, EventArgs e) => _fileService.SaveAs();
+        private void Save(object sender, EventArgs e) => _fileService.Save();
+        private void Exit(object sender, EventArgs e) => Close();
 
-                if (dialog.ShowDialog() == DialogResult.OK)
-                {
-                    _filePath = dialog.FileName;
-                    _db.Load(_filePath);
-                    RefreshAllContent();
-                }
-            }
-        }
-
-        private void SaveAs(object sender, EventArgs e)
-        {
-            using (var dialog = new SaveFileDialog())
-            {
-                dialog.Filter = $"Comparatist DataBase files (*{Extension})|*{Extension}";
-                dialog.Title = "Save Comparatist DataBase as...";
-
-                if (dialog.ShowDialog() == DialogResult.OK)
-                {
-                    string path = dialog.FileName;
-                    _db.Save(path);
-                    _filePath = path;
-                }
-            }
-        }
-
-        private void Save(object sender, EventArgs e)
-        {
-            if (_db == null)
-                MessageBox.Show("Nothing to save!");
-            else if (string.IsNullOrEmpty(_filePath))
-                SaveAs(sender, e);
-            else
-                _db.Save(_filePath);
-
-        }
-
-        private void Exit(object sender, EventArgs e)
-        {
-            Close();
-        }
-        #endregion FILE_OPERATIONS
-
-        #region COMMON_CONTENT
-        private void OnDataGridViewRightClick(object? sender, DataGridViewCellMouseEventArgs e)
-        {
-            if (sender is not DataGridView gridView)
-                return;
-
-            if (e.RowIndex >= 0 && e.Button == MouseButtons.Right)
-            {
-                gridView.ClearSelection();
-                gridView.Rows[e.RowIndex].Selected = true;
-                gridView.CurrentCell = gridView.Rows[e.RowIndex].Cells[e.ColumnIndex];
-            }
-        }
-
-        private void RefreshAllContent()
-        {
-            ShowActiveContentHolder();
-            SetCheckedRepositoryMenu();
-            RefreshRoots();
-            _semanticTreeService.Refresh();
-            _languagesService.Refresh();
-            _sourcesService.Refresh();
-        }
-
-        private void SetCheckedRepositoryMenu()
-        {
-            rootsToolStripMenuItem.Checked = _currentContentHolder == ContentHolderTypes.Roots;
-            semanticGroupsToolStripMenuItem.Checked = _currentContentHolder == ContentHolderTypes.SemanticGroups;
-            languagesToolStripMenuItem.Checked = _currentContentHolder == ContentHolderTypes.Languages;
-            sourcesToolStripMenuItem.Checked = _currentContentHolder == ContentHolderTypes.Sources;
-        }
-
-        private void ShowActiveContentHolder()
-        {
-            _languagesGridView.Visible = _currentContentHolder == ContentHolderTypes.Languages;
-            //dataGridViewWords.Visible = repo == "Words";
-            //dataGridViewStems.Visible = repo == "Stems";
-            //dataGridViewRoots.Visible = repo == "Roots";
-            _semanticTreeView.Visible = _currentContentHolder == ContentHolderTypes.SemanticGroups;
-            _sourcesGridView.Visible = _currentContentHolder == ContentHolderTypes.Sources;
-        }
-
-        private void SelectRoots(object sender, EventArgs e)
-        {
-            _currentContentHolder = ContentHolderTypes.Roots;
-            RefreshAllContent();
-        }
-
-        private void SelectSemanticGroups(object sender, EventArgs e)
-        {
-            _currentContentHolder = ContentHolderTypes.SemanticGroups;
-            RefreshAllContent();
-        }
-
-        private void SelectLanguages(object sender, EventArgs e)
-        {
-            _currentContentHolder = ContentHolderTypes.Languages;
-            RefreshAllContent();
-        }
-
-        private void SelectSources(object sender, EventArgs e)
-        {
-            _currentContentHolder = ContentHolderTypes.Sources;
-            RefreshAllContent();
-        }
-
-        #endregion COMMON_CONTENT
+        private void SelectRoots(object sender, EventArgs e) => SelectContent(ContentHolderTypes.Roots);
+        private void SelectSemanticGroups(object sender, EventArgs e) => SelectContent(ContentHolderTypes.SemanticGroups);
+        private void SelectLanguages(object sender, EventArgs e) => SelectContent(ContentHolderTypes.Languages);
+        private void SelectSources(object sender, EventArgs e) => SelectContent(ContentHolderTypes.Sources);
 
         private void AddSource(object sender, EventArgs e) => _sourcesService.Add();
         private void EditSource(object sender, EventArgs e) => _sourcesService.Edit();
@@ -158,11 +49,34 @@ namespace Comparatist
         private void MoveGroup(object sender, EventArgs e) => _semanticTreeService.Move();
         private void DeleteGroup(object sender, EventArgs e) => _semanticTreeService.Delete();
 
-        #region ROOTS
-        private void RefreshRoots()
+        private void RefreshAllContent()
         {
-
+            ShowActiveContentHolder();
+            SetCheckedRepositoryMenu();
+            _semanticTreeService.Refresh();
+            _languagesService.Refresh();
+            _sourcesService.Refresh();
         }
-        #endregion ROOTS
+
+        private void SetCheckedRepositoryMenu()
+        {
+            rootsToolStripMenuItem.Checked = _currentContentHolder == ContentHolderTypes.Roots;
+            semanticGroupsToolStripMenuItem.Checked = _currentContentHolder == ContentHolderTypes.SemanticGroups;
+            languagesToolStripMenuItem.Checked = _currentContentHolder == ContentHolderTypes.Languages;
+            sourcesToolStripMenuItem.Checked = _currentContentHolder == ContentHolderTypes.Sources;
+        }
+
+        private void ShowActiveContentHolder()
+        {
+            _languagesGridView.Visible = _currentContentHolder == ContentHolderTypes.Languages;
+            _semanticTreeView.Visible = _currentContentHolder == ContentHolderTypes.SemanticGroups;
+            _sourcesGridView.Visible = _currentContentHolder == ContentHolderTypes.Sources;
+        }
+
+        private void SelectContent(ContentHolderTypes type)
+        {
+            _currentContentHolder = type;
+            RefreshAllContent();
+        }
     }
 }
